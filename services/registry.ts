fc5b356c62bbca4b22bc24630a433f42a600f117
Santiago-pe <penasantiago346@gmail.com>
2024-06-02 14:01:18 -0300

    ADD: add uuid library to generate userId and get success statis code when registry endpoints is called

--- services/registry.ts
+++ services/registry.ts
@@ -2,6 +2,7 @@
 import AWS from 'aws-sdk';
 import bcrypt from 'bcrypt';
 import { APIGatewayProxyResult } from 'aws-lambda';
+import { v4 as uuidv4 } from 'uuid';
 import { buildResponse } from '../utils/utils';
 
 // Configuring AWS SDK
@@ -15,47 +16,12 @@ const userTable = "auth-microservices-users";
 
 // Interface for User object
 interface User {
+  userId: string;
   name: string;
   email: string;
   userName: string;
   password: string;
 }
-
-// Function to register a user
-export const register = async (userInfo: User): Promise<APIGatewayProxyResult> => {
-  const { name, email, password, userName } = userInfo;
-
-  if (!name || !email || !password || !userName) {
-    return buildResponse(401, {
-      message: "All fields are required",
-    });
-  }
-
-  const dynamoUser = await getUser(userName);
-  if (dynamoUser && dynamoUser.userName) {
-    return buildResponse(401, {
-      message: "User name already exists in our database. Please choose a different user name",
-    });
-  }
-
-  const encryptedPW = bcrypt.hashSync(password.trim(), 10);
-  const user: User = {
-    name: name,
-    email: email,
-    userName: userName.toLowerCase().trim(),
-    password: encryptedPW,
-  };
-
-  const saveUserResponse = await saveUser(user);
-  if (!saveUserResponse) {
-    return buildResponse(501, {
-      message: "Server Error. Please try again later",
-    });
-  }
-
-  return buildResponse(200, { userName: userName });
-};
-
 // Function to get a user from DynamoDB
 async function getUser(userName: string): Promise<AWS.DynamoDB.DocumentClient.AttributeMap | undefined> {
   const params = {
@@ -89,3 +55,40 @@ async function saveUser(user: User): Promise<boolean> {
     return false;
   }
 }
+
+// Function to register a user
+export const register = async (userInfo: User): Promise<APIGatewayProxyResult> => {
+  const { name, email, password, userName } = userInfo;
+
+  if (!name || !email || !password || !userName) {
+    return buildResponse(401, {
+      message: "All fields are required",
+    });
+  }
+
+  const dynamoUser = await getUser(userName);
+  if (dynamoUser && dynamoUser.userName) {
+    return buildResponse(401, {
+      message: "User name already exists in our database. Please choose a different user name",
+    });
+  }
+
+  const encryptedPW = bcrypt.hashSync(password.trim(), 10);
+  const user: User = {
+    userId: uuidv4(), 
+    name: name,
+    email: email,
+    userName: userName.toLowerCase().trim(),
+    password: encryptedPW,
+  };
+
+  const saveUserResponse = await saveUser(user);
+  if (!saveUserResponse) {
+    return buildResponse(501, {
+      message: "Server Error. Please try again later",
+    });
+  }
+
+  return buildResponse(200, { userName: userName });
+};
+
